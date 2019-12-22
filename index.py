e4e58551941428e62d82b20c9d15c47db5565f7f
gautamsood15 <gauty22@gmail.com>
2019-12-22 13:38:53 +0530

    creating code to run the application

--- index.py
+++ index.py
@@ -3,13 +3,23 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 import sys
 
-import os
-import os.path
 from PyQt5.uic import loadUiType
 
 
-ui = loadUiType('main.ui')
+ui , _ = loadUiType('main.ui')    #convert UI file to python file
 
 class MainApp(QMainWindow , ui):
     def __init__(self, parent=None):
-        
+        super(MainApp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+def main():
+    app = QApplication(sys.argv)     #application can have many windows, this is an object of QApplication
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    main()
