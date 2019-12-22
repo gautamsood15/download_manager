d74e57699b80fec5bd59ad78873f6ceec58e9b5d
gautamsood15 <gauty22@gmail.com>
2019-12-22 16:19:49 +0530

    adding extra icon + building functions tmeplate

--- index.py
+++ index.py
@@ -5,17 +5,36 @@ import sys
 
 from PyQt5.uic import loadUiType
 
+ui, _ = loadUiType('main.ui')  # convert UI file to python file
 
-ui , _ = loadUiType('main.ui')    #convert UI file to python file
 
-class MainApp(QMainWindow , ui):
+class MainApp(QMainWindow, ui):
     def __init__(self, parent=None):
         super(MainApp, self).__init__(parent)
         QMainWindow.__init__(self)
         self.setupUi(self)
 
+    def InitUI(self):    # contain all ui changes in loading
+        pass
+
+    def Handel_Buttons(self):  # handel all buttons in the app
+        pass
+
+    def Handle_progress(self):  # calculate the progress
+        pass
+
+    def Handle_Browse(self):  # enable browsing to our os , pick save location
+        pass
+
+    def Download(self):  # downloading any file
+        pass
+
+    def Save_Browse(self):  # save location in the line edit
+        pass
+
+
 def main():
-    app = QApplication(sys.argv)     #application can have many windows, this is an object of QApplication
+    app = QApplication(sys.argv)  # application can have many windows, this is an object of QApplication
     window = MainApp()
     window.show()
     app.exec_()
